8b2976cfb727d0ec5a5a8c27eb6931561f108f27
kartik <karrv95@gmail.com>
2023-01-17 20:06:22 -0500

    Created base template by adding block extends and layout.html -> which is the base since it will consist of everything that will appear in all pages like nav bar and footer. index.html will extend the layout by adding contents to it which done by using block content. Anything inside the block content will appear along with the layout and be a complete page

--- App/routes.py
+++ App/routes.py
@@ -7,16 +7,16 @@ from flask import render_template
 def index():
     return render_template("index.html", login=True)
 
-@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
-def index():
-    return render_template("index.html", login=True)
+@app.route("/login") # Can stack how many ever routing patterns as we want in the same format
+def login():
+    return render_template("login.html", login=True)
 
-@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
-def index():
-    return render_template("index.html", login=True)
+@app.route("/courses") # Can stack how many ever routing patterns as we want in the same format
+def courses():
+    return render_template("course.html", login=True)
 
-@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
-def index():
-    return render_template("index.html", login=True)
+@app.route("/register") # Can stack how many ever routing patterns as we want in the same format
+def register():
+    return render_template("register.html", login=True)
 
 
