28820b2a11d2af0823376f783ec30c18f0587b8e
kartik <karrv95@gmail.com>
2023-01-17 19:29:26 -0500

    Made changes and additions to index.html and routes.py and nav.html for url links

--- App/routes.py
+++ App/routes.py
@@ -3,5 +3,20 @@ from flask import render_template
 
 @app.route("/") # These are called function decorators
 @app.route("/index")
+@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
 def index():
-    return render_template("index.html")
+    return render_template("index.html", login=True)
+
+@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
+def index():
+    return render_template("index.html", login=True)
+
+@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
+def index():
+    return render_template("index.html", login=True)
+
+@app.route("/home") # Can stack how many ever routing patterns as we want in the same format
+def index():
+    return render_template("index.html", login=True)
+
+
