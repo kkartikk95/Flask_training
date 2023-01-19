b64e07f08fcee745ca5fd7614a94f1500ef1b1cd
kartik <karrv95@gmail.com>
2023-01-19 10:43:19 -0500

    Made changes by parsing data in routes.py for courses(added the data to return), changed courses.html to run in for loop to populate the data, made the nav link based on conditon by adding true condition to nav.html and routes.py

--- App/routes.py
+++ App/routes.py
@@ -5,7 +5,7 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home") # Can stack how many ever routing patterns as we want in the same format
 def index():
-    return render_template("index.html", login=True)
+    return render_template("index.html", index=True)
 
 @app.route("/login") # Can stack how many ever routing patterns as we want in the same format
 def login():
@@ -13,10 +13,19 @@ def login():
 
 @app.route("/courses") # Can stack how many ever routing patterns as we want in the same format
 def courses():
-    return render_template("courses.html", login=True)
+    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP",
+                   "credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1",
+                    "description":"Intro to Java Programming","credits":4,"term":"Spring"},
+                  {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming",
+                   "credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1",
+                    "description":"Intro to Angular","credits":3,"term":"Fall, Spring"},
+                  {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming",
+                   "credits":4,"term":"Fall"}]
+
+    return render_template("courses.html", courseData=courseData, courses=True)
 
 @app.route("/register") # Can stack how many ever routing patterns as we want in the same format
 def register():
-    return render_template("register.html", login=True)
+    return render_template("register.html", register=True)
 
 
