3608013879ca82ac6d3e36d48145a14b12fa0589
Kartik <karrv95@gmail.com>
2023-01-22 13:39:31 -0500

    Made modifications to config.py and init.py to connect the database MongoDB to flask

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
-
+from config import Config
+from flask_mongoengine import MongoEngine
 app = Flask(__name__) #This will make the current file as the main entry point
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
 
 from App import routes
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KY = os.environ.get("SECRET_KEY") or "secret_string" # Secret key for security reasons like cookies etc.
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret_string"
+    # Secret key for security reasons like cookies etc.
+
+    MONGODB_SETTINGS = {'db' : 'Flask_MongoDB_training'}
